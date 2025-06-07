479575ab8f72dae3a37ed1944f0824a403ca5375
Michael Jonathan Amay <michaeljonathanamay@lpc-depaulsecure-245-228.depaulsecure-student.depaul.edu>
2025-06-07 16:57:08 -0500

    second commit

--- src/main/java/com/example/demo/PayrollApplication.java
+++ src/main/java/com/example/demo/PayrollApplication.java
@@ -3,9 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PayrollApplication {
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+@SpringBootApplication
+public class PayrollApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PayrollApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
 	}
